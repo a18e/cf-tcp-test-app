4f3b19f59e02ad86b4ec1d31307fb47b9afa7098
Dominik Froehlich <dominik.froehlich@sap.com>
2020-10-23 16:22:33 +0200

    allow killing a specific instance

--- main.go
+++ main.go
@@ -17,27 +17,32 @@ func main() {
 		port = "8080"
 	}
 
+	index, _ := os.LookupEnv("CF_INSTANCE_INDEX")
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/fail", func(w http.ResponseWriter, req *http.Request) {
 
 		q := req.URL.Query()
-		p, err := strconv.Atoi(q.Get("p"))
+		p, _ := strconv.Atoi(q.Get("p"))
 		r := rand.Intn(100)
-		if err == nil {
-			if p < 0 {
-				p = 0
-			}
-			if p > 100 {
-				p = 100
-			}
-			if r < p {
-				log.Fatalf("%d < %d - CRASH!\n", r, p)
-			}
+		if p < 0 {
+			p = 0
+		}
+		if p > 100 {
+			p = 100
+		}
+		if r < p {
+			log.Fatalf("%d < %d - CRASH!\n", r, p)
+		}
+
+		i := q.Get("i")
+		if i == index {
+			log.Fatalf("instance %s = i %s - CRASH!\n", index, i)
 		}
 
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("%d >= %d - OK\n", r, p)))
-		log.Printf("%d >= %d - OK\n", r, p)
+		w.Write([]byte(fmt.Sprintf("(instance: %s) %d >= %d - OK\n", index, r, p)))
+		log.Printf("(instance: %s) %d >= %d - OK\n", index, r, p)
 	})
 
 	log.Printf("Listening on :%s", port)
