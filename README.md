6e2a2749e9533a895475b1755a33e1a0c5e36456
Alexander Nicke <alexander.nicke@sap.com>
2023-05-10 14:59:42 +0200

    DRAIN_DELAY added Code Cleanup

--- README.md
+++ README.md
@@ -38,17 +38,19 @@ Use `health-check-type: process` for [process healthchecks](https://docs.cloudfo
 
 The following environment variables affect the apps behaviour and can be specified in the manifest (see example).
 
-### `START_DELAY` and `STOP_DELAY`
-If a duration is specified here (e.g. "10s", "5m"), the app will wait for the specified amount
-- before listening (on port `8080` by default) during startup (`START_DELAY`)
-- before exiting after receiving a `SIGINT` or `SIGTERM` signal. (`STOP_DELAY`)
-### `INSTANT_CLOSE_LISTENER`
-If `INSTANT_CLOSE_LISTENER` is set to `true`, the app will stop listening on port 8080 immediately after `SIGINT` or `SIGTERM` is received.
-
-It will still wait the amount of time specified in `STOP_DELAY` before completely exiting.
 
 ### `INITIAL_HEALTH`
 If `INITIAL_HEALTH: false` is specified, the app will start with failing Health endpoint (i.e. `cf push` will fail)
+
+### Customizable Wait Periods 
+
+```mermaid
+flowchart LR
+    start["Process started"]--->|START_DELAY|crash["SIGTERM received"]--->|DRAIN_DELAY|drain["listener stopped"]--->|STOP_DELAY|stop["Process stopped"]
+    
+```
+
+`START_DELAY`, `DRAIN_DELAY` and `STOP_DELAY` can be set to durations, e.g. "10s", "5m".
 
 ## How to use
 After pushing the app to CF, use `cf logs go-tcp-test` to see the detailed log output of every request/health-check/event.
--- main.go
+++ main.go
@@ -15,13 +15,10 @@ import (
 
 var appIsHealthy = true
 var startDelay = 0 * time.Second
+var drainDelay = 0 * time.Second
 var stopDelay = 0 * time.Second
 
-// immediately closes the listener after app has become unhealthy
-var instantCloseListener = false
-
 func main() {
-	var err error
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "8080"
@@ -32,37 +29,21 @@ func main() {
 		appIsHealthy = strings.ToLower(initialHealth) == "true"
 	}
 
-	closeListenerString, ok := os.LookupEnv("INSTANT_CLOSE_LISTENER")
-	if ok {
-		instantCloseListener = strings.ToLower(closeListenerString) == "true"
-	}
+	durationFromEnv("START_DELAY", &startDelay)
+	durationFromEnv("DRAIN_DELAY", &drainDelay)
+	durationFromEnv("STOP_DELAY", &stopDelay)
 
-	startDelayString, ok := os.LookupEnv("START_DELAY")
-	if ok {
-		startDelay, err = time.ParseDuration(startDelayString)
-		if err != nil {
-			log.Fatalf("invalid START_STOP_DELAY %s", startDelayString)
-		}
-	}
-	stopDelayString, ok := os.LookupEnv("STOP_DELAY")
-	if ok {
-		stopDelay, err = time.ParseDuration(stopDelayString)
-		if err != nil {
-			log.Fatalf("invalid START_STOP_DELAY %s", stopDelayString)
-		}
-	}
 	log.Printf("waiting %s for startup\n", startDelay)
 	time.Sleep(startDelay)
 	log.Printf("done waiting")
 
 	log.Printf("Listening on :%s | app-health: %t", port, appIsHealthy)
-	setupTcpServer()
-
+	setupTcpServer(port)
 }
 
-func setupTcpServer() {
+func setupTcpServer(port string) {
 	// Listen for incoming connections on port 8080
-	ln, err := net.Listen("tcp", ":8080")
+	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		log.Println(err)
 		return
@@ -90,6 +71,17 @@ func setupTcpServer() {
 	waitGroup.Wait()
 }
 
+func durationFromEnv(envVar string, duration *time.Duration) {
+	var err error
+	durationString, ok := os.LookupEnv(envVar)
+	if ok {
+		*duration, err = time.ParseDuration(durationString)
+		if err != nil {
+			log.Fatalf("invalid %s: %s", envVar, durationString)
+		}
+	} // else keep default
+}
+
 func setupDelayedShutdown(ln net.Listener, group *sync.WaitGroup) {
 	defer group.Done()
 
@@ -101,17 +93,16 @@ func setupDelayedShutdown(ln net.Listener, group *sync.WaitGroup) {
 		case sig := <-sigChan:
 			log.Printf("Received signal %v...", sig)
 
-			if instantCloseListener {
-				log.Printf("closing listener")
-				err := ln.Close()
-				if err != nil {
-					log.Print(err)
-				}
-			} else {
-				log.Printf("keeping listener open")
+			log.Printf("draining for %v", drainDelay)
+			time.Sleep(drainDelay)
+			log.Printf("done drainig. closing listener.")
+
+			err := ln.Close()
+			if err != nil {
+				log.Print(err)
 			}
 
-			log.Printf("sleeping for %v", stopDelay)
+			log.Printf("sleeping for %v before stop.", stopDelay)
 			time.Sleep(stopDelay)
 			log.Printf("done sleeping. exiting.")
 			syscall.Exit(0) // we need to syscall exit here in case the incoming connection loop doesn't receive any more connections and doesn't break
@@ -125,16 +116,13 @@ func getHttpPathAndMethod(requestBody []byte) (string, string, error) {
 	//requestLine:= strings.Split(string(requestBody), "\n")
 	re := regexp.MustCompile(`^(\S+)\s+(\S+)\s+HTTP/\d\.\d`)
 	matches := re.FindStringSubmatch(string(requestBody))
-
 	if len(matches) < 3 {
 		return "", "", fmt.Errorf("could not parse HTTP request %s", string(requestBody))
 	}
 
 	method := matches[1]
 	path := matches[2]
-
 	return path, method, nil
-
 }
 
 func handleConnection(conn net.Conn) {
@@ -163,18 +151,22 @@ func handleConnection(conn net.Conn) {
 		} else {
 			conn.Close()
 		}
+
 	case "/togglehealth":
 		appIsHealthy = !appIsHealthy
 		log.Printf("received toggle-health request. New health: %t", appIsHealthy)
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\napp healthy: %t", appIsHealthy)
 		conn.Write([]byte(response))
+
 	case "/drop":
 		log.Printf("received drop request. New health: %t", appIsHealthy)
 		conn.Close()
+
 	case "/always_up":
 		log.Printf("received always_up request: %t", appIsHealthy)
 		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!\r\n"
 		conn.Write([]byte(response))
+
 	default:
 		log.Printf("received request: method: %s, path: %s | app-health: %t", method, path, appIsHealthy)
 		if appIsHealthy {
